c3895146c8a7fcd740489c87df0d97ac0b705b69
Alexander Koc <alexander.koc@skane.se>
2022-11-24 11:17:00 +0100

    Set up extensions

---
+++ app/extensions.py
@@ -0,0 +1,13 @@
+"""
+This module stores variables/objects that need to be accessed all over
+the app. e.g. mongo : MongoClient.
+"""
+
+Uncomment those that are needed:
+# from flask_login import LoginManager
+# login_manager = LoginManager()
+
+# from flask_pymongo import PyMongo
+# mongo = PyMongo()
+
+
