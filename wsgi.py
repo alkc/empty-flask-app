a96383559637a8d8d256f22e570c71a62a9b1244
Alexander Koc <alexander.koc@skane.se>
2022-11-24 11:17:14 +0100

    Configure wsgi.py for flask run and gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,14 @@
 from app import create_app
+
+app = create_app()
+
+if __name__ != "__main__":
+    print("Setting up Gunicorn logging.")
+    import logging
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+
