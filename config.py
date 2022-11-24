9367eb9b8feea4fb819c2377c1f9333963c7fe78
Alexander Koc <alexander.koc@skane.se>
2022-11-24 10:56:32 +0100

    Skeletons!

---
+++ config.py
@@ -0,0 +1,10 @@
+class DefaultConfig():
+    SECRET_KEY = None
+    # MONGO_URI="localhost:27017"
+
+class DevelopmentConfig():
+    SECRET_KEY = "ChangeMe1234"
+    # MONGO_URI="localhost:27017"
+
+class TestingConfig():
+    pass
