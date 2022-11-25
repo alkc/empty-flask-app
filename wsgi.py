e3fa9290095c5c73f6f21336532206883156e117
Alexander Koc <alexander.koc@skane.se>
2022-11-25 11:16:48 +0100

    Detect gunicorn and configure log handlers accordingly

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,24 @@
 from app import create_app
+import os
+
+server_software = os.environ.get('SERVER_SOFTWARE', '')
+is_gunicorn = "gunicorn" in server_software
 
 app = create_app()
 
+print(
+     "SERVER_SOFTWARE: "
+    f"{server_software}"
+)
+
+
 if __name__ != "__main__":
-    print("Setting up Gunicorn logging.")
-    import logging
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
+    if is_gunicorn:
+        print("Setting up Gunicorn logging.")
+        import logging
+        gunicorn_logger = logging.getLogger('gunicorn.error')
+        app.logger.handlers = gunicorn_logger.handlers
+        app.logger.setLevel(gunicorn_logger.level)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
